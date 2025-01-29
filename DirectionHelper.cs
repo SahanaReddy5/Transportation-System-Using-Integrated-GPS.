3cff561dcc78ed66b1d24b7556dab334ee2d5f80
SahanaReddy5 <79832276+SahanaReddy5@users.noreply.github.com>
2025-01-28 21:00:02 -0500

    Add files via upload

---
+++ DirectionHelper.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TransportSystemClient.Models
+{
+    public class DirectionHelper
+    {
+        // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+        public class GeocodedWaypoint
+        {
+            [JsonProperty("geocoder_status")]
+            public string GeocoderStatus { get; set; }
+
+            [JsonProperty("place_id")]
+            public string PlaceId { get; set; }
+
+            [JsonProperty("types")]
+            public List<string> Types { get; set; }
+        }
+
+        public class Northeast
+        {
+            [JsonProperty("lat")]
+            public double Lat { get; set; }
+
+            [JsonProperty("lng")]
+            public double Lng { get; set; }
+        }
+
+        public class Southwest
+        {
+            [JsonProperty("lat")]
+            public double Lat { get; set; }
+
+            [JsonProperty("lng")]
+            public double Lng { get; set; }
+        }
+
+        public class Bounds
+        {
+            [JsonProperty("northeast")]
+            public Northeast Northeast { get; set; }
+
+            [JsonProperty("southwest")]
+            public Southwest Southwest { get; set; }
+        }
+
+        public class Distance
+        {
+            [JsonProperty("text")]
+            public string Text { get; set; }
+
+            [JsonProperty("value")]
+            public int Value { get; set; }
+        }
+
+        public class Duration
+        {
+            [JsonProperty("text")]
+            public string Text { get; set; }
+
+            [JsonProperty("value")]
+            public int Value { get; set; }
+        }
+
+        public class EndLocation
+        {
+            [JsonProperty("lat")]
+            public double Lat { get; set; }
+
+            [JsonProperty("lng")]
+            public double Lng { get; set; }
+        }
+
+        public class StartLocation
+        {
+            [JsonProperty("lat")]
+            public double Lat { get; set; }
+
+            [JsonProperty("lng")]
+            public double Lng { get; set; }
+        }
+
+        public class Polyline
+        {
+            [JsonProperty("points")]
+            public string Points { get; set; }
+        }
+
+        public class Step
+        {
+            [JsonProperty("distance")]
+            public Distance Distance { get; set; }
+
+            [JsonProperty("duration")]
+            public Duration Duration { get; set; }
+
+            [JsonProperty("end_location")]
+            public EndLocation EndLocation { get; set; }
+
+            [JsonProperty("html_instructions")]
+            public string HtmlInstructions { get; set; }
+
+            [JsonProperty("polyline")]
+            public Polyline Polyline { get; set; }
+
+            [JsonProperty("start_location")]
+            public StartLocation StartLocation { get; set; }
+
+            [JsonProperty("travel_mode")]
+            public string TravelMode { get; set; }
+
+            [JsonProperty("maneuver")]
+            public string Maneuver { get; set; }
+        }
+
+        public class Leg
+        {
+            [JsonProperty("distance")]
+            public Distance Distance { get; set; }
+
+            [JsonProperty("duration")]
+            public Duration Duration { get; set; }
+
+            [JsonProperty("end_address")]
+            public string EndAddress { get; set; }
+
+            [JsonProperty("end_location")]
+            public EndLocation EndLocation { get; set; }
+
+            [JsonProperty("start_address")]
+            public string StartAddress { get; set; }
+
+            [JsonProperty("start_location")]
+            public StartLocation StartLocation { get; set; }
+
+            [JsonProperty("steps")]
+            public List<Step> Steps { get; set; }
+
+            [JsonProperty("traffic_speed_entry")]
+            public List<object> TrafficSpeedEntry { get; set; }
+
+            [JsonProperty("via_waypoint")]
+            public List<object> ViaWaypoint { get; set; }
+        }
+
+        public class OverviewPolyline
+        {
+            [JsonProperty("points")]
+            public string Points { get; set; }
+        }
+
+        public class Route
+        {
+            [JsonProperty("bounds")]
+            public Bounds Bounds { get; set; }
+
+            [JsonProperty("copyrights")]
+            public string Copyrights { get; set; }
+
+            [JsonProperty("legs")]
+            public List<Leg> Legs { get; set; }
+
+            [JsonProperty("overview_polyline")]
+            public OverviewPolyline OverviewPolyline { get; set; }
+
+            [JsonProperty("summary")]
+            public string Summary { get; set; }
+
+            [JsonProperty("warnings")]
+            public List<object> Warnings { get; set; }
+
+            [JsonProperty("waypoint_order")]
+            public List<object> WaypointOrder { get; set; }
+        }
+
+        public class Directionsmodel
+        {
+            [JsonProperty("geocoded_waypoints")]
+            public List<GeocodedWaypoint> GeocodedWaypoints { get; set; }
+
+            [JsonProperty("routes")]
+            public List<Route> Routes { get; set; }
+
+            [JsonProperty("status")]
+            public string Status { get; set; }
+        }
+
+    }
+}
---
+++ RestServices.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using TransportSystemClient.Models;
+
+namespace TransportSystemClient
+{
+    public class RestServices
+    {
+
+        //Gets all users from the  api & deserializes the Json response
+        async public Task<UserModel[]> GetUserdata()
+        {
+            UserSerialize users;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string s = await client.GetStringAsync(RestConstants.UserEndpoint);
+                users = JsonConvert.DeserializeObject<UserSerialize>(s);
+            }
+            return users.Data.ToArray();
+        }
+        //posts the serialized user object to  the api & deserializes the Json response
+        async public Task<string> PostUserdata(UserModel user)
+        {
+            string responsemessage = string.Empty;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            StringContent content = new StringContent(JsonConvert.SerializeObject(user));
+            content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var result = await client.PostAsync(RestConstants.UserEndpoint, content).ConfigureAwait(false);
+                if (result.IsSuccessStatusCode)
+                {
+                    responsemessage = await result.Content.ReadAsStringAsync();
+                }
+            }
+            var msg = JObject.Parse(responsemessage);
+            return msg["Data"].ToString();
+        }
+        //gets all drivers list and deserializes the json response 
+        async public Task<DriverModel[]> GetDriverdata()
+        {
+            DriverSerialize Drivers;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string s = await client.GetStringAsync(RestConstants.DriverEndpoint);
+                Drivers = JsonConvert.DeserializeObject<DriverSerialize>(s);
+
+            }
+
+            return Drivers.Data.ToArray();
+        }
+        //posts the serialized driver object to the api 
+        async public Task<string> PostDriverdata(DriverModel d)
+        {
+            string responsemessage = string.Empty;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            var content = new StringContent(JsonConvert.SerializeObject(d));
+            content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var result = await client.PostAsync(RestConstants.DriverEndpoint, content).ConfigureAwait(false);
+                if (result.IsSuccessStatusCode)
+                {
+                    responsemessage = await result.Content.ReadAsStringAsync();
+                }
+            }
+            var msg = JObject.Parse(responsemessage);
+            return msg["Data"].ToString();
+        }
+        //gets the driver & bus details for a specific bus
+        async public Task<DriverModel> GetDetailsByBusno(string busno)
+        {
+            DriverModel Drivers = new DriverModel(); DriverInfo driverdata;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                string s = await client.GetStringAsync(RestConstants.DriverEndpoint + "?Busno=" + busno);
+                driverdata = JsonConvert.DeserializeObject<DriverInfo>(s);
+            }
+
+            return driverdata.Data;
+        }
+        //updates the drivers details based on the given driver id & driver object
+        async public Task<string> UpdateDriverdata(int driverid,DriverModel d)
+        {
+            string responsemessage = string.Empty;
+            ServicePointManager.Expect100Continue = true;
+            System.Net.ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls | SecurityProtocolType.Ssl3;
+            HttpClientHandler httpClientHandler = new HttpClientHandler();
+            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+            var content = new StringContent(JsonConvert.SerializeObject(d));
+            content.Headers.ContentType = new MediaTypeWithQualityHeaderValue("application/json");
+            using (HttpClient client = new HttpClient(httpClientHandler))
+            {
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var result = await client.PutAsync(RestConstants.DriverEndpoint+"?DriverId="+driverid, content).ConfigureAwait(false);
+                if (result.IsSuccessStatusCode)
+                {
+                    responsemessage = await result.Content.ReadAsStringAsync();
+                }
+            }
+            var msg = JObject.Parse(responsemessage);
+            return msg["Data"].ToString();
+        }
+    }
+}
